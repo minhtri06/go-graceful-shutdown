b0b325aeaf60b0e6934da40d0f878e8c994646ec
tripm <tripm@aipower.com.vn>
2025-04-25 11:32:35 +0700

    add assertion.Error() helper

--- assertion/assertion.go
+++ assertion/assertion.go
@@ -1,6 +1,9 @@
 package assertion
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func Equal[T comparable](t testing.TB, got, expect T) {
 	t.Helper()
@@ -13,5 +16,12 @@ func NoError(t testing.TB, err error) {
 	t.Helper()
 	if err != nil {
 		t.Errorf("didn't expect an error, but got one: %v", err)
+	}
+}
+
+func Error(t testing.TB, got, expect error) {
+	t.Helper()
+	if !errors.Is(got, expect) {
+		t.Errorf("expect: %v\ngot: %v", expect, got)
 	}
 }
